1aee2abd5ed55b14984d4093cf90e1a580ef62a2
odmark2023@gmail.com <odmark2023@gmail.com>
2026-01-08 10:08:20 +0200

    building the class ticket

--- entities/ticket.js
+++ entities/ticket.js
@@ -1,2 +1,14 @@
-class Ticket{}
-class VIPTicket extends Ticket{}
+class Ticket{
+    constructor(ticketNumber, price, ownerName){
+        this.ticketNumber = ticketNumber
+        this.price = price
+        this.ownerName = ownerName
+    }
+}
+class VIPTicket extends Ticket{
+    #benefitsList
+    constructor(ticketNumber, price, ownerName){
+        super(ticketNumber, price, ownerName)
+        this.#benefitsList = ["Free Alcohol", "Free Food ", "Hot Towels "]
+    }
+}
