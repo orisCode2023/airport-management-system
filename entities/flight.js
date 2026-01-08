ab084bb0be3938d993594d8a5791c1634c8d64b5
odmark2023@gmail.com <odmark2023@gmail.com>
2026-01-08 09:37:39 +0200

    flight class is ready

--- entities/flight.js
+++ entities/flight.js
@@ -1,3 +1,11 @@
 class Flight {
     
+    #MaximumNumberOfPassengers
+    constructor(flightName, airlineFlightNumber, regularTicketPrice, VIPTicketPrice){
+        this.flightName = flightName
+        this.airlineFlightNumber = airlineFlightNumber
+        this.regularTicketPrice = regularTicketPrice
+        this.VIPTicketPrice = VIPTicketPrice
+        TicketsList = new Array(this.#MaximumNumberOfPassengers)
+    }
 }
